8c13297bb05ff01bf0a770fa142f07e80c62a6f7
Steven Engler <smengler3@gmail.com>
2024-05-22 13:52:04 -0400

    Improved torrent list details

--- src/template_helpers.rs
+++ src/template_helpers.rs
@@ -1,3 +1,22 @@
-pub fn as_f64(x: u64) -> f64 {
-    x as f64
+pub fn json_num_to_bool(val: &serde_json::Value) -> Option<bool> {
+    match val {
+        serde_json::Value::Bool(x) => Some(*x),
+        serde_json::Value::Number(x) => {
+            #[allow(clippy::manual_map)]
+            if let Some(x) = x.as_u64() {
+                Some(x != 0)
+            } else if let Some(x) = x.as_i64() {
+                Some(x != 0)
+            } else if let Some(x) = x.as_f64() {
+                Some(x != 0.0)
+            } else {
+                None
+            }
+        }
+        _ => None,
+    }
+}
+
+pub fn identity_copy<T: Copy>(x: &T) -> T {
+    *x
 }
